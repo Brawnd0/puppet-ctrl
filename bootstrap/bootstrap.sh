5d0c0d07b36f410c83f5150f474f0c7eba4f3fac
Brandon Evans <brandon.evans@gmail.com>
2015-07-03 12:44:39 -0700

    chicken or the egg?

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -16,14 +16,17 @@
 # Add bootstrap modules
 mkdir -p /root/bootstrap/modules
 puppet module install --modulepath=/root/bootstrap/modules zack/r10k --version 2.5.8
-puppet module install --modulepath=/root/bootstrap/modules stahnma/epel --version 1.0.2
-puppet module install --modulepath=/root/bootstrap/modules stephenrjohnson/puppet --version 1.3.1
-puppet module install --modulepath=/root/bootstrap/modules hunner/hiera --version 1.1.1
+#puppet module install --modulepath=/root/bootstrap/modules stahnma/epel --version 1.0.2
+#puppet module install --modulepath=/root/bootstrap/modules stephenrjohnson/puppet --version 1.3.1
+#puppet module install --modulepath=/root/bootstrap/modules abstractit-puppet --version 1.3.1
+#puppet module install --modulepath=/root/bootstrap/modules hunner/hiera --version 1.1.1
 
 # Configure the master, hiera, and r10k services
-puppet apply --modulepath=/root/bootstrap/modules master.pp && \
-puppet apply --modulepath=/root/bootstrap/modules hiera.pp && \
-puppet apply --modulepath=/root/bootstrap/modules r10k_installation.pp && \
+#puppet apply --modulepath=/root/bootstrap/modules master.pp && \
+#  puppet apply --modulepath=/root/bootstrap/modules hiera.pp && \
+#  puppet apply --modulepath=/root/bootstrap/modules r10k.pp && \
+
+
 # If everything went well, deploy using r10k
 r10k deploy environment -p
 
