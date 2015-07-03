1bc59a1af1e6f17c584c2356a2815e7b393ef2e3
Brandon Evans <brandon.evans@gmail.com>
2015-07-03 12:03:46 -0700

    add begining of bootstrap script

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#
+# Goal : self contained puppet 3.8 repository 
+#   - Enable development on laptop
+#   - Push to upstream to rollout to production
+#   - Minimal initial puppet module requirements ( zack/r10k + abstractit/puppet )
+#   
+#
+#
+
+# Install r10k using Gem
+# Launch r10k
+#   - install abstractit/puppet module
+#   - 
+
+# Add bootstrap modules
+mkdir -p /root/bootstrap/modules
+puppet module install --modulepath=/root/bootstrap/modules zack/r10k --version 2.5.8
+puppet module install --modulepath=/root/bootstrap/modules stahnma/epel --version 1.0.2
+puppet module install --modulepath=/root/bootstrap/modules stephenrjohnson/puppet --version 1.3.1
+puppet module install --modulepath=/root/bootstrap/modules hunner/hiera --version 1.1.1
+
+# Configure the master, hiera, and r10k services
+puppet apply --modulepath=/root/bootstrap/modules master.pp && \
+puppet apply --modulepath=/root/bootstrap/modules hiera.pp && \
+puppet apply --modulepath=/root/bootstrap/modules r10k_installation.pp && \
+# If everything went well, deploy using r10k
+r10k deploy environment -p
+
+# If everything is successful, run puppet, otherwise alert
+if [ $? -eq 0 ]
+then
+  puppet agent -t
+  chkconfig puppet on
+  service puppet start
+else
+  echo "Some part of the bootstrap process failed. Investigate the errors and proceed with manual bootstrapping."
+  echo ""
+  echo "See https://github.com/puppetinabox/documentation#bootstrap for the steps."
+  echo ""
+fi
