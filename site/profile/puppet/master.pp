0148de5304199487f03d0498c18cdbf0fe883674
Brandon Evans <brandon.evans@gmail.com>
2015-07-03 12:23:25 -0700

    testing out master deploy

---
+++ site/profile/puppet/master.pp
@@ -0,0 +1,15 @@
+#
+#
+#
+#
+#
+#
+#
+
+class { '::puppet::profile::master':
+    server_type    => 'puppetserver',
+    basemodulepath => '/etc/puppet/modules:site',
+    java_ram       => '2g',
+    server_version => '3.8.1',
+}
+
