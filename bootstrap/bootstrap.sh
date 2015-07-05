013aa8b54df3ab00340d12c7881384e6fb2e14ef
Brandon Evans <brandon.evans@gmail.com>
2015-07-05 11:22:42 -0700

    working on bootstrap script

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -27,7 +27,6 @@ fi
 rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
 yum install -y git rubygems
 gem install bundle
-#gem install --source 'https://rubygems.org' bundler
 
 TOPDIR=`git rev-parse --show-toplevel`
 
@@ -35,24 +34,25 @@ cd ${TOPDIR}/
 bundle install --path=vendor/bundle --binstubs=bin/
 
 # Add bootstrap modules
-mkdir -p /root/bootstrap/modules
-./bin/puppet module install --modulepath=/root/bootstrap/modules zack/r10k --version 2.8.2
-#./bin/puppet module install --modulepath=/root/bootstrap/modules maestrodev/rvm --version 1.12.0
-./bin/puppet module install --modulepath=/root/bootstrap/modules hunner/hiera --version 1.1.1
-./bin/puppet module install --modulepath=/root/bootstrap/modules stahnma/epel --version 1.0.2
-./bin/puppet module install --modulepath=/root/bootstrap/modules puppetlabs/puppetdb --version 4.3.0
-./bin/puppet module install --modulepath=/root/bootstrap/modules abstractit-puppet --version 1.3.1
+MODPATH=$$TOPDIR/tmp/bootstrap/modules
+mkdir -p $MODPATH
+./bin/puppet module install --modulepath=$MODPATH zack/r10k --version 2.8.2
+#./bin/puppet module install --modulepath=$MODPATH maestrodev/rvm --version 1.12.0
+./bin/puppet module install --modulepath=$MODPATH hunner/hiera --version 1.1.1
+./bin/puppet module install --modulepath=$MODPATH stahnma/epel --version 1.0.2
+./bin/puppet module install --modulepath=$MODPATH puppetlabs/puppetdb --version 4.3.0
+./bin/puppet module install --modulepath=$MODPATH abstractit-puppet --version 1.3.1
 
 
 exit
-#puppet module install --modulepath=/root/bootstrap/modules stephenrjohnson/puppet --version 1.3.1
-#puppet module install --modulepath=/root/bootstrap/modules abstractit-puppet --version 1.3.1
-#puppet module install --modulepath=/root/bootstrap/modules hunner/hiera --version 1.1.1
+#puppet module install --modulepath=$MODPATH stephenrjohnson/puppet --version 1.3.1
+#puppet module install --modulepath=$MODPATH abstractit-puppet --version 1.3.1
+#puppet module install --modulepath=$MODPATH hunner/hiera --version 1.1.1
 
 # Configure the master, hiera, and r10k services
-#puppet apply --modulepath=/root/bootstrap/modules master.pp && \
-puppet apply --modulepath=/root/bootstrap/modules hiera.pp && \
-  puppet apply --modulepath=/root/bootstrap/modules r10k.pp
+#puppet apply --modulepath=$MODPATH master.pp && \
+puppet apply --modulepath=$MODPATH hiera.pp && \
+  puppet apply --modulepath=$MODPATH r10k.pp
 
 
 # If everything went well, deploy using r10k
