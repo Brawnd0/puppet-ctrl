7be21ba75c2b1e9d5a6a5ba1d155f281519f538f
Brandon Evans <brandon.evans@gmail.com>
2015-07-06 23:03:18 -0700

    add modules to puppetfile

--- bootstrap/master.pp
+++ bootstrap/master.pp
@@ -1,18 +1,11 @@
-Package {
-      allow_virtual => true,
-}
-
-include ::epel
-Yumrepo<| |> -> Package <| |>
-class { '::puppet::profile::master':
-    server_type    => 'puppetserver',
-    basemodulepath => '/etc/puppet/modules:site',
-    java_ram       => '2g',
-    server_version => '3.8.1',
-}
-
-#include ::puppetdb
-#class { '::puppet::master':
-#      storeconfigs   => true,
-#        environments => directory,
+#Package {
+#  allow_virtual => true,
 #}
+
+#include ::epel
+#Yumrepo<| |> -> Package <| |>
+#include ::puppetdb
+class { '::puppet::profile::master':
+  basemodulepath => '/etc/puppet/modules:site',
+  java_ram => '1532M'
+}
