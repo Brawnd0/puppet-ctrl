19b8568326a09460869d64ace7a6d11a8dc268b8
Brandon Evans <brandon.evans@gmail.com>
2015-07-06 23:35:12 -0700

    working on master bootstrap

--- bootstrap/master.pp
+++ bootstrap/master.pp
@@ -5,7 +5,19 @@
 #include ::epel
 #Yumrepo<| |> -> Package <| |>
 #include ::puppetdb
-class { '::puppet::profile::master':
-  basemodulepath => '/etc/puppet/modules:site',
-  java_ram => '1532M'
+class { '::puppet::master':
+  server_type               => 'puppetserver',
+  server_package            => 'puppet-server', 
+  server_version            => '3.8.1',
+  java_ram                  => '1532M',
+  environmentpath           => "${codedir}/environments",
+  basemodulepath            => "${confdir}/modules:/usr/share/puppet/modules:site",
+  hiera_eyaml_key_directory => "${codedir}/hiera_eyaml_keys",
+  hieradata_path            => "${confdir}/hieradata",
+  hiera_backends            => {
+    'yaml' => {
+      'datadir' => '/etc/puppet/hieradata/%{environment}'
+    }
+  },
+
 }
